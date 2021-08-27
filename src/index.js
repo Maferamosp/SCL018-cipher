bee5c18cd59adb46d1b59b83d9f09c036599fd26
Maferamosp <ramosmariaf@hotmail.com>
2021-08-27 11:12:19 -0400

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let containerAnswer = document.getElementById('mensaje2')
+
+document.getElementById('cifrar').addEventListener('click', () => {
+    let toEncode = document.getElementById('mensaje1').value;
+    let desplazamiento = document.getElementById('offset').value;
+    containerAnswer.innerHTML = cipher.encode(toEncode, desplazamiento);
+})
+document.getElementById('descifrar').addEventListener('click', () =>{
+    let toDecode = document.getElementById('mensaje1').value;
+    let desplazamiento = document.getElementById('offset').value;
+    containerAnswer.innerHTML = cipher.decode(toDecode, desplazamiento)
+})
