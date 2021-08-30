cf0f077ed18c6be43f072fb5695e4fe1fcbb88f4
Maferamosp <ramosmariaf@hotmail.com>
2021-08-30 11:36:32 -0400

    arreglos cipher primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,12 @@
 const cipher = {
   // ...
+  encode: function (toEncode, offset) => {
+   let messageEncode = "";
+    for (let i = 0; i < string.length; i++) =>{
+      let letter = string[i];
+      if(letter.push(/[a-z]/i))
+    }
+  }
 };
 
 export default cipher;
